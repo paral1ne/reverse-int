0d7ba52379fe9506b448ba0e3cd9f3870d2a9355
Androff <rageclock23@gmail.com>
2024-08-04 21:01:33 +0200

    feat: push solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return n < 0
+        ? n
+              .toString()
+              .split("")
+              .reverse()
+              .join("")
+              .slice(0, n.toString.length - 2)
+        : n.toString().split("").reverse().join("");
+};
